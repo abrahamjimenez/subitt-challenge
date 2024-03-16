8c4c8a158fdb1e67a57ed052cf655250b94cb36f
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-16 12:51:01 -0600

    Data is fetched

--- frontend/src/components/UserRegistration.jsx
+++ frontend/src/components/UserRegistration.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 const UserRegistration = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get("http://localhost:8080/")
+            setProducts(data)
+        }
+
+        fetchProducts()
+    }, []);
+
+    useEffect(() => {
+        console.log(products)
+    }, [products]);
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <label htmlFor="name">Name</label>
             <input type="text" name="name" id="name"/>
 
@@ -11,6 +31,8 @@ const UserRegistration = () => {
 
             <label htmlFor="password">Password</label>
             <input type="password" id="password" name="password"/>
+
+            <button type="submit">Submit</button>
         </form>
     );
 };
