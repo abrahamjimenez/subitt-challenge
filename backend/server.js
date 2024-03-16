8310fea7bbcb48c6108b93b9bd20824b1175fafb
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-16 12:32:28 -0600

    Can push user to users

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+const PORT = 8080;
+
+const app = express();
+
+// JSON middleware
+app.use(express.json())
+
+const users = [
+    {name: "john", email: "john@email.com", password: "john123"}, {
+    name: "jane",
+    email: "jane@email.com",
+    password: "jane123",
+}];
+
+app.get("/", (req, res) => {
+    res.send(users);
+});
+
+app.post("/", (req, res) => {
+    res.status(201).send("Created user")
+    users.push(req.body)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}/`);
+});
+
